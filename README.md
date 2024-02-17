c260093c41b8daa2716f4397eb645477b0b50fbc
Martin Costello <martin@martincostello.com>
2024-02-17 11:25:19 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# CHANGE_ME
+
+[![Build status](https://github.com/martincostello/CHANGE_ME/workflows/build/badge.svg?branch=main&event=push)](https://github.com/martincostello/CHANGE_ME/actions?query=workflow%3Abuild+branch%3Amain+event%3Apush)
+
+## Introduction
+
+CHANGE_ME
+
+## Building and Testing
+
+Compiling the application yourself requires Git and the
+[.NET SDK](https://dotnet.microsoft.com/en-us/download "Download the .NET SDK")
+to be installed.
+
+To build and test the application locally from a terminal/command-line, run the
+following set of commands:
+
+```powershell
+git clone https://github.com/martincostello/CHANGE_ME.git
+cd CHANGE_ME
+./build.ps1
+```
+
+## Feedback
+
+Any feedback or issues can be added to the issues for this project in
+[GitHub](https://github.com/martincostello/CHANGE_ME/issues "Issues for this project on GitHub.com").
+
+## Repository
+
+The repository is hosted in
+[GitHub](https://github.com/martincostello/CHANGE_ME "This project on GitHub.com"):
+https://github.com/martincostello/CHANGE_ME.git
+
+## License
+
+This project is licensed under the
+[Apache 2.0](http://www.apache.org/licenses/LICENSE-2.0.txt "The Apache 2.0 license")
+license.
---
+++ build.ps1
@@ -0,0 +1,139 @@
+#! /usr/bin/env pwsh
+
+#Requires -PSEdition Core
+#Requires -Version 7
+
+param(
+    [Parameter(Mandatory = $false)][switch] $SkipTests
+)
+
+if ($null -eq $env:MSBUILDTERMINALLOGGER) {
+  $env:MSBUILDTERMINALLOGGER = "auto"
+}
+
+$ErrorActionPreference = "Stop"
+$ProgressPreference = "SilentlyContinue"
+
+$solutionPath = $PSScriptRoot
+$sdkFile = Join-Path $solutionPath "global.json"
+
+$dotnetVersion = (Get-Content $sdkFile | Out-String | ConvertFrom-Json).sdk.version
+
+$installDotNetSdk = $false;
+
+if (($null -eq (Get-Command "dotnet" -ErrorAction SilentlyContinue)) -and ($null -eq (Get-Command "dotnet.exe" -ErrorAction SilentlyContinue))) {
+    Write-Host "The .NET SDK is not installed."
+    $installDotNetSdk = $true
+}
+else {
+    Try {
+        $installedDotNetVersion = (dotnet --version 2>&1 | Out-String).Trim()
+    }
+    Catch {
+        $installedDotNetVersion = "?"
+    }
+
+    if ($installedDotNetVersion -ne $dotnetVersion) {
+        Write-Host "The required version of the .NET SDK is not installed. Expected $dotnetVersion."
+        $installDotNetSdk = $true
+    }
+}
+
+if ($installDotNetSdk -eq $true) {
+
+    $env:DOTNET_INSTALL_DIR = Join-Path $PSScriptRoot ".dotnetcli"
+    $sdkPath = Join-Path $env:DOTNET_INSTALL_DIR "sdk" $dotnetVersion
+
+    if (!(Test-Path $sdkPath)) {
+        if (!(Test-Path $env:DOTNET_INSTALL_DIR)) {
+            mkdir $env:DOTNET_INSTALL_DIR | Out-Null
+        }
+        [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor "Tls12"
+        if (($PSVersionTable.PSVersion.Major -ge 6) -And !$IsWindows) {
+            $installScript = Join-Path $env:DOTNET_INSTALL_DIR "install.sh"
+            Invoke-WebRequest "https://dot.net/v1/dotnet-install.sh" -OutFile $installScript -UseBasicParsing
+            chmod +x $installScript
+            & $installScript --version "$dotnetVersion" --install-dir "$env:DOTNET_INSTALL_DIR" --no-path --skip-non-versioned-files
+        }
+        else {
+            $installScript = Join-Path $env:DOTNET_INSTALL_DIR "install.ps1"
+            Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -OutFile $installScript -UseBasicParsing
+            & $installScript -Version "$dotnetVersion" -InstallDir "$env:DOTNET_INSTALL_DIR" -NoPath -SkipNonVersionedFiles
+        }
+    }
+}
+else {
+    $env:DOTNET_INSTALL_DIR = Split-Path -Path (Get-Command dotnet).Path
+}
+
+$dotnet = Join-Path "$env:DOTNET_INSTALL_DIR" "dotnet"
+
+if ($installDotNetSdk -eq $true) {
+    $env:PATH = "$env:DOTNET_INSTALL_DIR;$env:PATH"
+}
+
+function DotNetPack {
+    param([string]$Project)
+
+    & $dotnet pack $Project --include-symbols --include-source
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "dotnet pack failed with exit code $LASTEXITCODE"
+    }
+}
+
+function DotNetTest {
+    param([string]$Project)
+
+    $additionalArgs = @()
+
+    if (![string]::IsNullOrEmpty($env:GITHUB_SHA)) {
+        $additionalArgs += "--logger"
+        $additionalArgs += "GitHubActions;report-warnings=false"
+    }
+
+    & $dotnet test $Project --configuration "Release" $additionalArgs
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "dotnet test failed with exit code $LASTEXITCODE"
+    }
+}
+
+function DotNetPublish {
+    param([string]$Project)
+
+    & $dotnet publish $Project
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "dotnet publish failed with exit code $LASTEXITCODE"
+    }
+}
+
+$packageProjects = @(
+    (Join-Path $solutionPath "src" "CHANGE_ME" "CHANGE_ME.csproj")
+)
+
+$publishProjects = @(
+    (Join-Path $solutionPath "src" "CHANGE_ME" "CHANGE_ME.csproj")
+)
+
+$testProjects = @(
+    (Join-Path $solutionPath "tests" "CHANGE_ME.Tests" "CHANGE_ME.Tests.csproj")
+)
+
+Write-Host "Publishing solution..." -ForegroundColor Green
+ForEach ($project in $publishProjects) {
+    DotNetPublish $project $Configuration
+}
+
+Write-Host "Packaging libraries..." -ForegroundColor Green
+ForEach ($project in $packageProjects) {
+    DotNetPack $project $Configuration
+}
+
+if ($SkipTests -eq $false) {
+    Write-Host "Testing $($testProjects.Count) project(s)..." -ForegroundColor Green
+    ForEach ($project in $testProjects) {
+        DotNetTest $project
+    }
+}
