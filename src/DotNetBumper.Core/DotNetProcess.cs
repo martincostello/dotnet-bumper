e13eac688afaca55c0b5a3580d6c59b8ba726933
martincostello <martin@martincostello.com>
2024-02-18 16:38:13 +0000

    Fix deadlock
    Fix deadlock when `--verbose` isn't used with `--test` due to the output buffer being full.

--- src/DotNetBumper.Core/DotNetProcess.cs
+++ src/DotNetBumper.Core/DotNetProcess.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using System.Diagnostics;
+using System.Text;
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Extensions.Logging;
 
@@ -35,22 +36,38 @@ public sealed partial class DotNetProcess(ILoggerFactory loggerFactory)
         using var exited = new CancellationTokenSource();
         using var combined = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, exited.Token);
 
-        if (_logger.IsEnabled(LogLevel.Debug))
+        var debug = _logger.IsEnabled(LogLevel.Debug);
+
+        var command = process.StartInfo.ArgumentList[0];
+        var logger = loggerFactory.CreateLogger($"dotnet {command}");
+
+        // See https://stackoverflow.com/a/16326426/1064169
+        var output = new StringBuilder();
+        process.OutputDataReceived += (_, args) =>
         {
-            process.Exited += (_, _) => exited.Cancel();
+            if (args.Data is { } message)
+            {
+                if (debug)
+                {
+                    if (!string.IsNullOrWhiteSpace(message))
+                    {
+                        Log.CommandOutput(logger, args.Data);
+                    }
+                }
+                else
+                {
+                    output.Append(message);
+                }
+            }
+        };
 
-            var command = process.StartInfo.ArgumentList[0];
-            var outputLogger = loggerFactory.CreateLogger($"dotnet {command}");
-
-            StreamLogs(process.StandardOutput, outputLogger, LogLevel.Debug, combined.Token);
-            StreamLogs(process.StandardError, outputLogger, LogLevel.Error, combined.Token);
-        }
+        process.BeginOutputReadLine();
 
         await process.WaitForExitAsync(cancellationToken);
 
         if (process.ExitCode != 0)
         {
-            await Log.LogCommandFailedAsync(_logger, process);
+            await Log.LogCommandFailedAsync(_logger, process, output.ToString());
             return false;
         }
 
@@ -74,54 +91,13 @@ public sealed partial class DotNetProcess(ILoggerFactory loggerFactory)
         return Process.Start(startInfo) ?? throw new InvalidOperationException($"Failed to start process for dotnet {arguments[0]}.");
     }
 
-    private static void StreamLogs(
-        StreamReader output,
-        ILogger logger,
-        LogLevel level,
-        CancellationToken cancellationToken)
-    {
-        _ = Task.Factory.StartNew(
-            async (state) =>
-            {
-                var args = ((StreamReader Output, ILogger Logger, LogLevel Level, CancellationToken Token))state!;
-                await ReadStreamAsync(args.Output, args.Logger, args.Level, args.Token);
-            },
-            (output, logger, level, cancellationToken),
-            cancellationToken,
-            TaskCreationOptions.LongRunning,
-            TaskScheduler.Default);
-
-        static async Task ReadStreamAsync(
-            StreamReader output,
-            ILogger logger,
-            LogLevel level,
-            CancellationToken cancellationToken)
-        {
-            try
-            {
-                while (!cancellationToken.IsCancellationRequested)
-                {
-                    string? line = await output.ReadLineAsync(cancellationToken);
-
-                    if (line is not null)
-                    {
-#pragma warning disable CA1848 // Use the LoggerMessage delegates
-                        logger.Log(level, "{Message}", line);
-#pragma warning restore CA1848 // Use the LoggerMessage delegates
-                    }
-                }
-            }
-            catch (OperationCanceledException)
-            {
-                // Ignore
-            }
-        }
-    }
-
     [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
     private static partial class Log
     {
-        public static async Task LogCommandFailedAsync(ILogger logger, Process process)
+        public static async Task LogCommandFailedAsync(
+            ILogger logger,
+            Process process,
+            string output)
         {
             string command = process.StartInfo.ArgumentList[0];
 
@@ -129,7 +105,6 @@ public sealed partial class DotNetProcess(ILoggerFactory loggerFactory)
 
             if (!logger.IsEnabled(LogLevel.Debug))
             {
-                string output = await process.StandardOutput.ReadToEndAsync(CancellationToken.None);
                 string error = await process.StandardError.ReadToEndAsync(CancellationToken.None);
 
                 if (!string.IsNullOrEmpty(output))
@@ -146,43 +121,25 @@ public sealed partial class DotNetProcess(ILoggerFactory loggerFactory)
 
         [LoggerMessage(
             EventId = 1,
-            Level = LogLevel.Debug,
-            Message = "Upgrading NuGet package versions.")]
-        public static partial void UpgradingPackages(ILogger logger);
-
-        [LoggerMessage(
-            EventId = 2,
-            Level = LogLevel.Information,
-            Message = "Upgraded {Count} NuGet package(s).")]
-        public static partial void UpgradedPackages(ILogger logger, int count);
-
-        [LoggerMessage(
-            EventId = 3,
-            Level = LogLevel.Debug,
-            Message = "Restored NuGet packages for {Directory}.")]
-        public static partial void RestoredPackages(ILogger logger, string directory);
-
-        [LoggerMessage(
-            EventId = 4,
-            Level = LogLevel.Warning,
-            Message = "Unable to restore NuGet packages for {Directory}.")]
-        public static partial void UnableToRestorePackages(ILogger logger, string directory);
-
-        [LoggerMessage(
-            EventId = 5,
             Level = LogLevel.Warning,
             Message = "Command \"dotnet {Command}\" failed with exit code {ExitCode}.")]
         public static partial void CommandFailed(ILogger logger, string command, int exitCode);
 
         [LoggerMessage(
-            EventId = 6,
+            EventId = 2,
+            Level = LogLevel.Debug,
+            Message = "{Output}.")]
+        public static partial void CommandOutput(ILogger logger, string output);
+
+        [LoggerMessage(
+            EventId = 3,
             Level = LogLevel.Warning,
             Message = "Command \"dotnet {Command}\" standard output: {Output}",
             SkipEnabledCheck = true)]
         public static partial void CommandFailedOutput(ILogger logger, string command, string output);
 
         [LoggerMessage(
-            EventId = 7,
+            EventId = 4,
             Level = LogLevel.Warning,
             Message = "Command \"dotnet {Command}\" standard error: {Error}")]
         public static partial void CommandFailedError(ILogger logger, string command, string error);
