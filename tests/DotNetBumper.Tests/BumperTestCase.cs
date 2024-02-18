56a57e7b4a94cc14ea8c9fd8ce92dacd3a245725
martincostello <martin@martincostello.com>
2024-02-18 09:41:40 +0000

    Improve test case rendering
    Add custom class for test cases.

---
+++ tests/DotNetBumper.Tests/BumperTestCase.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Text;
+
+namespace MartinCostello.DotNetBumper;
+
+public sealed class BumperTestCase(
+    string sdkVersion,
+    IList<string>? targetFrameworks = null,
+    IDictionary<string, string>? packageReferences = null,
+    IList<string>? args = null)
+{
+    public string SdkVersion { get; } = sdkVersion;
+
+    public IList<string> TargetFrameworks { get; } = targetFrameworks ?? [];
+
+    public IDictionary<string, string> PackageReferences { get; } = packageReferences ?? new Dictionary<string, string>(0);
+
+    public IList<string> Arguments { get; } = args ?? [];
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder().Append(CultureInfo.InvariantCulture, $"SDK: {SdkVersion}");
+
+        if (TargetFrameworks is { Count: > 0 } tfms)
+        {
+            builder.Append(CultureInfo.InvariantCulture, $"; TFM{(tfms.Count is 1 ? string.Empty : "s")}: [")
+                   .Append(string.Join(", ", tfms))
+                   .Append(']');
+        }
+
+        if (PackageReferences is { Count: > 0 } packages)
+        {
+            builder.Append("; Packages: [")
+                   .Append(string.Join(", ", packages.Keys))
+                   .Append(']');
+        }
+
+        if (Arguments is { Count: > 0 } args)
+        {
+            builder.Append("; Args: [")
+                   .Append(string.Join(", ", args))
+                   .Append(']');
+        }
+
+        return builder.ToString();
+    }
+}
