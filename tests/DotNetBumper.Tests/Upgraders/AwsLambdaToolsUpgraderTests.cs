b39bec8d1cf0ef3869d9b22200f8b88ec1f2a163
martincostello <martin@martincostello.com>
2024-02-21 23:23:48 +0000

    Test AWS and VS components for BOM
    Add unit tests for verify AWS Lambda defaults and Visual Studio Components preserve the BOM.

--- tests/DotNetBumper.Tests/Upgraders/AwsLambdaToolsUpgraderTests.cs
+++ tests/DotNetBumper.Tests/Upgraders/AwsLambdaToolsUpgraderTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using System.Text;
 using System.Text.Json;
 using Microsoft.Extensions.Options;
 using Spectre.Console.Testing;
@@ -173,5 +174,77 @@ public class AwsLambdaToolsUpgraderTests(ITestOutputHelper outputHelper)
 
         // Assert
         actual.ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData("\n", false)]
+    [InlineData("\n", true)]
+    [InlineData("\r", false)]
+    [InlineData("\r", true)]
+    [InlineData("\r\n", false)]
+    [InlineData("\r\n", true)]
+    public async Task UpgradeAsync_Preserves_Bom(string newLine, bool bom)
+    {
+        // Arrange
+        string[] originalLines =
+        [
+            "{",
+            "  \"framework\": \"net6.0\"",
+            "}",
+        ];
+
+        string[] expectedLines =
+        [
+            "{",
+            "  \"framework\": \"net10.0\"",
+            "}",
+        ];
+
+        string fileContents = string.Join(newLine, originalLines) + newLine;
+        string expectedContent = string.Join(Environment.NewLine, expectedLines) + Environment.NewLine;
+
+        using var fixture = new UpgraderFixture(outputHelper);
+
+        var encoding = new UTF8Encoding(bom);
+        string dockerfile = await fixture.Project.AddFileAsync("aws-lambda-tools-defaults.json", fileContents, encoding);
+
+        var upgrade = new UpgradeInfo()
+        {
+            Channel = Version.Parse("10.0"),
+            EndOfLife = DateOnly.MaxValue,
+            ReleaseType = DotNetReleaseType.Lts,
+            SdkVersion = new($"10.0.100"),
+            SupportPhase = DotNetSupportPhase.Active,
+        };
+
+        var options = Options.Create(new UpgradeOptions() { ProjectPath = fixture.Project.DirectoryName });
+        var logger = outputHelper.ToLogger<AwsLambdaToolsUpgrader>();
+        var target = new AwsLambdaToolsUpgrader(fixture.Console, options, logger);
+
+        // Act
+        ProcessingResult actualUpdated = await target.UpgradeAsync(upgrade, CancellationToken.None);
+
+        // Assert
+        actualUpdated.ShouldBe(ProcessingResult.Success);
+
+        string actualContent = await File.ReadAllTextAsync(dockerfile);
+        actualContent.ShouldBe(expectedContent);
+
+        byte[] actualBytes = await File.ReadAllBytesAsync(dockerfile);
+
+        if (bom)
+        {
+            actualBytes.ShouldStartWithUTF8Bom();
+        }
+        else
+        {
+            actualBytes.ShouldNotStartWithUTF8Bom();
+        }
+
+        // Act
+        actualUpdated = await target.UpgradeAsync(upgrade, CancellationToken.None);
+
+        // Assert
+        actualUpdated.ShouldBe(ProcessingResult.None);
     }
 }
