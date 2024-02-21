5bf9602e2579a08764d8e53aaf7e180ee2d2657e
martincostello <martin@martincostello.com>
2024-02-21 17:30:01 +0000

    Allow JSON comments and commas
    Configure the `JsonNode` parsing options to allow trailing commas and comments.
    Resolves #45.

--- src/DotNetBumper.Core/JsonHelpers.cs
+++ src/DotNetBumper.Core/JsonHelpers.cs
@@ -2,16 +2,23 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 
 namespace MartinCostello.DotNetBumper;
 
 internal static class JsonHelpers
 {
+    internal static readonly JsonDocumentOptions DocumentOptions = new()
+    {
+        AllowTrailingCommas = true,
+        CommentHandling = JsonCommentHandling.Skip,
+    };
+
     public static bool TryLoadObject(string path, [NotNullWhen(true)] out JsonObject? root)
     {
         using var stream = File.OpenRead(path);
-        root = JsonNode.Parse(stream) as JsonObject;
+        root = JsonNode.Parse(stream, documentOptions: DocumentOptions) as JsonObject;
         return root is not null;
     }
 }
