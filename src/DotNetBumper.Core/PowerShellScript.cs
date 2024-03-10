aaa88f743f59a6d3293b8a57d3f679b0b101d755
martincostello <martin@martincostello.com>
2024-03-10 15:02:54 -0700

    Update PowerShell scripts
    Apply target framework and runtime identifier edits in PowerShell scripts.
    Relates to #39.

---
+++ src/DotNetBumper.Core/PowerShellScript.cs
@@ -0,0 +1,158 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Management.Automation.Language;
+using MartinCostello.DotNetBumper.Upgraders;
+
+namespace MartinCostello.DotNetBumper;
+
+internal sealed class PowerShellScript
+{
+    private PowerShellScript(
+        string fileName,
+        FileMetadata fileMetadata,
+        List<string> lines,
+        Ast syntaxTree)
+    {
+        FileName = fileName;
+        FileMetadata = fileMetadata;
+        Lines = lines;
+        SyntaxTree = syntaxTree;
+    }
+
+    public string FileName { get; }
+
+    public FileMetadata FileMetadata { get; }
+
+    public List<string> Lines { get; }
+
+    public Ast SyntaxTree { get; }
+
+    public static async Task<PowerShellScript?> TryParseAsync(string path, CancellationToken cancellationToken)
+    {
+        using var input = FileHelpers.OpenRead(path, out var metadata);
+        using var reader = new StreamReader(input, metadata.Encoding);
+
+        List<string> lines = [];
+        string? line;
+
+        while ((line = await reader.ReadLineAsync(cancellationToken)) is not null)
+        {
+            lines.Add(line);
+        }
+
+        string contents = string.Join(metadata.NewLine, lines);
+
+        var syntaxTree = Parser.ParseInput(contents, path, out _, out var errors);
+
+        if (errors.Length is not 0)
+        {
+            return null;
+        }
+
+        return new PowerShellScript(path, metadata, lines, syntaxTree);
+    }
+
+    public bool TryUpdate(Version channel)
+    {
+        var vistor = new SyntaxTreeVisitor(channel);
+        SyntaxTree.Visit(vistor);
+
+        if (vistor.Edits.Count is 0)
+        {
+            return false;
+        }
+
+        foreach (var lineEdits in vistor.Edits.GroupBy((p) => p.Location.StartLineNumber))
+        {
+            int lineIndex = lineEdits.Key - 1;
+
+            var edits = lineEdits
+                .OrderBy((p) => p.Location.StartOffset)
+                .ToList();
+
+            var builder = new StringBuilder();
+
+            int offset = 0;
+
+            var original = Lines[lineIndex];
+
+            foreach ((var location, var replacement) in edits)
+            {
+                int start = location.StartColumnNumber - 1;
+                int end = location.EndColumnNumber - 1;
+
+                builder.Append(original[offset..start]);
+                builder.Append(replacement);
+
+                offset = end;
+            }
+
+            builder.Append(original[offset..]);
+
+            Lines[lineIndex] = builder.ToString();
+        }
+
+        return true;
+    }
+
+    private sealed class SyntaxTreeVisitor(Version channel) : AstVisitor2
+    {
+        public List<(IScriptExtent Location, string Replacement)> Edits { get; } = [];
+
+        public override AstVisitAction VisitCommand(CommandAst commandAst)
+        {
+            foreach (var element in commandAst.CommandElements)
+            {
+                if (element is StringConstantExpressionAst constant)
+                {
+                    TryUpdateConstant(constant);
+                }
+            }
+
+            return base.VisitCommand(commandAst);
+        }
+
+        public override AstVisitAction VisitParameter(ParameterAst parameterAst)
+        {
+            if (parameterAst.DefaultValue is StringConstantExpressionAst constant)
+            {
+                TryUpdateConstant(constant);
+            }
+
+            return base.VisitParameter(parameterAst);
+        }
+
+        public override AstVisitAction VisitAssignmentStatement(AssignmentStatementAst assignmentStatementAst)
+        {
+            if (assignmentStatementAst.Right is CommandExpressionAst command &&
+                command.Expression is StringConstantExpressionAst constant)
+            {
+                TryUpdateConstant(constant);
+            }
+
+            return base.VisitAssignmentStatement(assignmentStatementAst);
+        }
+
+        private void TryUpdateConstant(StringConstantExpressionAst constant)
+        {
+            if (TargetFrameworkHelpers.TryUpdateTfm(constant.Value, channel, out var updated) ||
+                RuntimeIdentifierHelpers.TryUpdateRid(constant.Value, out updated))
+            {
+                char? quote = constant.StringConstantType switch
+                {
+                    StringConstantType.SingleQuoted or StringConstantType.SingleQuotedHereString => '\'',
+                    StringConstantType.DoubleQuoted or StringConstantType.DoubleQuotedHereString => '"',
+                    _ => null,
+                };
+
+                if (quote is { } value)
+                {
+                    updated = $"{value}{updated}{value}";
+                }
+
+                Edits.Add((constant.Extent, updated));
+            }
+        }
+    }
+}
---
+++ tests/DotNetBumper.Tests/Upgraders/PowerShellScriptUpgraderTests.cs
@@ -0,0 +1,186 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.DotNetBumper.Upgraders;
+
+public class PowerShellScriptUpgraderTests(ITestOutputHelper outputHelper)
+{
+    [Theory]
+    [ClassData(typeof(DotNetChannelTestData))]
+    public async Task UpgradeAsync_Upgrades_PowerShell_Script(string channel)
+    {
+        // Arrange
+        string fileContents =
+            """
+            #! /usr/bin/env pwsh
+
+            #Requires -PSEdition Core
+            #Requires -Version 7
+
+            param(
+                [Parameter(Mandatory = $false)][switch] $SkipTests,
+                [Parameter(Mandatory = $false)][string] $Configuration = "Release",
+                [Parameter(Mandatory = $false)][string] $Framework = "net6.0",
+                [Parameter(Mandatory = $false)][string] $Runtime = "win10-x64"
+            )
+
+            $ErrorActionPreference = "Stop"
+            $ProgressPreference = "SilentlyContinue"
+
+            dotnet publish --configuration $Configuration --framework $Framework --runtime $Runtime
+            """;
+
+        string expectedContents =
+            $"""
+             #! /usr/bin/env pwsh
+             
+             #Requires -PSEdition Core
+             #Requires -Version 7
+             
+             param(
+                 [Parameter(Mandatory = $false)][switch] $SkipTests,
+                 [Parameter(Mandatory = $false)][string] $Configuration = "Release",
+                 [Parameter(Mandatory = $false)][string] $Framework = "net{channel}",
+                 [Parameter(Mandatory = $false)][string] $Runtime = "win-x64"
+             )
+             
+             $ErrorActionPreference = "Stop"
+             $ProgressPreference = "SilentlyContinue"
+             
+             dotnet publish --configuration $Configuration --framework $Framework --runtime $Runtime
+             """;
+
+        using var fixture = new UpgraderFixture(outputHelper);
+
+        string script = await fixture.Project.AddFileAsync("build.ps1", fileContents);
+
+        var upgrade = new UpgradeInfo()
+        {
+            Channel = Version.Parse(channel),
+            EndOfLife = DateOnly.MaxValue,
+            ReleaseType = DotNetReleaseType.Lts,
+            SdkVersion = new($"{channel}.100"),
+            SupportPhase = DotNetSupportPhase.Active,
+        };
+
+        var target = CreateTarget(fixture);
+
+        // Act
+        ProcessingResult actualUpdated = await target.UpgradeAsync(upgrade, CancellationToken.None);
+
+        // Assert
+        actualUpdated.ShouldBe(ProcessingResult.Success);
+
+        string actualContent = await File.ReadAllTextAsync(script);
+        actualContent.TrimEnd().ShouldBe(expectedContents.TrimEnd());
+
+        // Act
+        actualUpdated = await target.UpgradeAsync(upgrade, CancellationToken.None);
+
+        // Assert
+        actualUpdated.ShouldBe(ProcessingResult.None);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("{}")]
+    [InlineData("<document/>")]
+    public async Task UpgradeAsync_Handles_Invalid_Scripts(string content)
+    {
+        // Arrange
+        using var fixture = new UpgraderFixture(outputHelper);
+
+        string script = await fixture.Project.AddFileAsync("script.ps1", content);
+
+        var upgrade = new UpgradeInfo()
+        {
+            Channel = new(8, 0),
+            EndOfLife = DateOnly.MaxValue,
+            ReleaseType = DotNetReleaseType.Lts,
+            SdkVersion = new("8.0.201"),
+            SupportPhase = DotNetSupportPhase.Active,
+        };
+
+        var target = CreateTarget(fixture);
+
+        // Act
+        ProcessingResult actual = await target.UpgradeAsync(upgrade, CancellationToken.None);
+
+        // Assert
+        actual.ShouldBe(ProcessingResult.None);
+    }
+
+    [Theory]
+    [ClassData(typeof(FileEncodingTestData))]
+    public async Task UpgradeAsync_Preserves_Line_Endings(string newLine, bool hasUtf8Bom)
+    {
+        // Arrange
+        string[] originalLines =
+        [
+            "#! /usr/bin/env pwsh",
+            string.Empty,
+            "dotnet publish --framework \"net6.0\" --runtime \"win10-x64\"",
+        ];
+
+        string[] expectedLines =
+        [
+            "#! /usr/bin/env pwsh",
+            string.Empty,
+            "dotnet publish --framework \"net10.0\" --runtime \"win-x64\"",
+        ];
+
+        string fileContents = string.Join(newLine, originalLines) + newLine;
+        string expectedContent = string.Join(newLine, expectedLines) + newLine;
+
+        using var fixture = new UpgraderFixture(outputHelper);
+
+        var encoding = new UTF8Encoding(hasUtf8Bom);
+        string script = await fixture.Project.AddFileAsync("script.ps1", fileContents, encoding);
+
+        var upgrade = new UpgradeInfo()
+        {
+            Channel = Version.Parse("10.0"),
+            EndOfLife = DateOnly.MaxValue,
+            ReleaseType = DotNetReleaseType.Lts,
+            SdkVersion = new("10.0.100"),
+            SupportPhase = DotNetSupportPhase.Active,
+        };
+
+        var target = CreateTarget(fixture);
+
+        // Act
+        ProcessingResult actualUpdated = await target.UpgradeAsync(upgrade, CancellationToken.None);
+
+        // Assert
+        actualUpdated.ShouldBe(ProcessingResult.Success);
+
+        string actualContent = await File.ReadAllTextAsync(script);
+        actualContent.ShouldBe(expectedContent);
+
+        byte[] actualBytes = await File.ReadAllBytesAsync(script);
+
+        if (hasUtf8Bom)
+        {
+            actualBytes.ShouldStartWithUTF8Bom();
+        }
+        else
+        {
+            actualBytes.ShouldNotStartWithUTF8Bom();
+        }
+
+        // Act
+        actualUpdated = await target.UpgradeAsync(upgrade, CancellationToken.None);
+
+        // Assert
+        actualUpdated.ShouldBe(ProcessingResult.None);
+    }
+
+    private static PowerShellScriptUpgrader CreateTarget(UpgraderFixture fixture)
+    {
+        return new(
+            fixture.Console,
+            fixture.Environment,
+            fixture.CreateOptions(),
+            fixture.CreateLogger<PowerShellScriptUpgrader>());
+    }
+}
