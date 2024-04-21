1f83741542b11c6d006b316f8c261b9ca6a5e36e
martincostello <martin@martincostello.com>
2024-04-21 14:55:53 +0100

    Increase timeout
    Increase test timeout to 2 minutes.

--- tests/DotNetBumper.Tests/Upgraders/DotNetCodeUpgraderTests.cs
+++ tests/DotNetBumper.Tests/Upgraders/DotNetCodeUpgraderTests.cs
@@ -5,7 +5,7 @@ namespace MartinCostello.DotNetBumper.Upgraders;
 
 public class DotNetCodeUpgraderTests(ITestOutputHelper outputHelper)
 {
-    private static TimeSpan Timeout { get; } = TimeSpan.FromMinutes(1);
+    private static TimeSpan Timeout { get; } = TimeSpan.FromMinutes(2);
 
     public static TheoryData<string> Channels()
     {
