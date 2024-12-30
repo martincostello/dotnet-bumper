6ceb356c05219e3215617a051a0b7528462632f2
martincostello <martin@martincostello.com>
2024-12-30 13:15:19 +0000

    Sort references
    Sort the package references.

--- tests/DotNetBumper.Tests/Project.cs
+++ tests/DotNetBumper.Tests/Project.cs
@@ -82,8 +82,8 @@ internal sealed class Project : IDisposable
         packageReferences ??= new Dictionary<string, string>()
         {
             ["Microsoft.NET.Test.Sdk"] = "17.12.0",
-            ["xunit.v3"] = "1.0.0",
             ["xunit.runner.visualstudio"] = "3.0.0",
+            ["xunit.v3"] = "1.0.0",
         };
 
         return await AddProjectAsync(path, targetFrameworks, packageReferences, projectReferences, "Exe");
