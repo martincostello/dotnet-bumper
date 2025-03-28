b7df5700571c0d44e087c61c4ffe94810222f81e
martincostello <martin@martincostello.com>
2025-03-28 17:14:57 +0000

    Initial daily build support
    Initial WIP for getting the latest daily build of .NET.
    
    Contributes to #650.

--- src/DotNetBumper.Core/DotNetUpgradeFinder.cs
+++ src/DotNetBumper.Core/DotNetUpgradeFinder.cs
@@ -32,6 +32,59 @@ public partial class DotNetUpgradeFinder(
     /// </returns>
     public virtual async Task<UpgradeInfo?> GetUpgradeAsync(CancellationToken cancellationToken)
     {
+        if (options.Value.UpgradeType is UpgradeType.Daily)
+        {
+            // TODO Make configurable?
+            var quality = DotNetQualities.Daily;
+
+            // TODO Automatically try to compute what the in-development version is
+            // TODO More robust
+            // TODO Need to handle roll-forward as the SDK is unlikely to be installed
+            // TODO Install the new SDK locally to avoid need to install?
+            var channel = new Version(options.Value.DotNetChannel ?? "10.0");
+
+            var versionUrl = $"https://aka.ms/dotnet/{channel.ToString(2)}/{quality}/sdk-productVersion.txt";
+
+            using var response = await httpClient.GetAsync(versionUrl, cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                // TODO Error handling
+                return null;
+            }
+
+            if (response.Content.Headers.ContentType?.MediaType is not ("application/octet-stream" or "text/plain"))
+            {
+                // TODO Error handling
+                return null;
+            }
+
+            var version = await response.Content.ReadAsStringAsync(cancellationToken);
+
+            if (!NuGetVersion.TryParse(version.Trim(), out var sdkVersion))
+            {
+                // TODO Error handling
+                return null;
+            }
+
+            // TODO Compute when Update Tuesday in November will be and set that as the date
+            var utcNow = DateOnly.FromDateTime(TimeProvider.System.GetUtcNow().UtcDateTime);
+            var endOfLife = new DateOnly(utcNow.Year, 11, 14);
+            if (endOfLife < utcNow)
+            {
+                endOfLife = endOfLife.AddYears(1);
+            }
+
+            return new()
+            {
+                Channel = channel,
+                EndOfLife = endOfLife,
+                ReleaseType = sdkVersion.Major % 2 is 0 ? DotNetReleaseType.Lts : DotNetReleaseType.Sts, // HACK Better way to determine?
+                SdkVersion = sdkVersion,
+                SupportPhase = DotNetSupportPhase.Preview,
+            };
+        }
+
         var candidates = await GetChannelsAsync(cancellationToken);
         return GetUpgrade(candidates);
     }
@@ -174,6 +227,14 @@ public partial class DotNetUpgradeFinder(
             latestChannel.ReleaseType);
 
         return latestChannel;
+    }
+
+    private static class DotNetQualities
+    {
+        internal const string Daily = "daily";
+        internal const string Signed = "signed";
+        internal const string Validated = "validated";
+        internal const string Preview = "preview";
     }
 
     [ExcludeFromCodeCoverage]
