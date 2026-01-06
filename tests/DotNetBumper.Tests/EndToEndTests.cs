e391964aee081f26c99882105c6124a547ed68fc
martincostello <martin@martincostello.com>
2026-01-06 14:49:05 +0000

    Skip daily build test in January
    - Skip daily builds in January before there's an active preview.
    - Update values ready for .NET 10 to 11.

--- tests/DotNetBumper.Tests/EndToEndTests.cs
+++ tests/DotNetBumper.Tests/EndToEndTests.cs
@@ -54,11 +54,11 @@ public sealed class EndToEndTests(
             ]);
         }
 
-        // Skip daily build test in November and December when one release
+        // Skip daily build test in January-December when one release
         // has just finished and the next hasn't ramped up yet.
-        if (TimeProvider.System.GetUtcNow().Month is not (11 or 12))
+        if (TimeProvider.System.GetUtcNow().Month is not (1 or 11 or 12))
         {
-            testCases.Add(new BumperTestCase("9.0.100", ["net9.0"], ["--upgrade-type=daily"], Packages(("Microsoft.Extensions.Configuration.UserSecrets", "9.0.0"))));
+            testCases.Add(new BumperTestCase("10.0.100", ["net10.0"], ["--upgrade-type=daily"], Packages(("Microsoft.Extensions.Configuration.UserSecrets", "10.0.0"))));
         }
 
         List<string> formats = ["Json", "Markdown"];
