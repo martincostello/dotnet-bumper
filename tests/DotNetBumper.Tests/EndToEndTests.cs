46c4d7608c613084ce213c93d2f4d96641536403
Martin Costello <martin@martincostello.com>
2026-02-11 21:45:17 +0000

    Update EndToEndTests.cs

--- tests/DotNetBumper.Tests/EndToEndTests.cs
+++ tests/DotNetBumper.Tests/EndToEndTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Copyright (c) Martin Costello, 2024. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using System.Text.Json;
@@ -156,12 +156,7 @@ public sealed class EndToEndTests(
 
         var actualConfig = await File.ReadAllTextAsync(vsconfig, fixture.CancellationToken);
 
-#if NET10_0_OR_GREATER
         var config = JsonElement.Parse(actualConfig);
-#else
-        var config = JsonDocument.Parse(actualConfig).RootElement;
-#endif
-
         config.TryGetProperty("components", out var property).ShouldBeTrue();
         property.ValueKind.ShouldBe(JsonValueKind.Array);
 
@@ -171,12 +166,7 @@ public sealed class EndToEndTests(
 
         var actualVscode = await File.ReadAllTextAsync(vscode, fixture.CancellationToken);
 
-#if NET10_0_OR_GREATER
         config = JsonElement.Parse(actualVscode);
-#else
-        config = JsonDocument.Parse(actualVscode).RootElement;
-#endif
-
         config.TryGetProperty("configurations", out property).ShouldBeTrue();
         property.ValueKind.ShouldBe(JsonValueKind.Array);
 
